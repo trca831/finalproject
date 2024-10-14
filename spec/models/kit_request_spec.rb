178bc2065cf59bf476cb525c8e7163989156febc
Marcia <juddhope@gmail.com>
2024-10-14 12:01:54 -0500

    Corrected model tests after adding traits to user, added user request testing.

--- spec/models/kit_request_spec.rb
+++ spec/models/kit_request_spec.rb
@@ -1,36 +1,36 @@
 require "rails_helper"
 
 RSpec.describe KitRequest, type: :model do
-  let(:user) { create(:user) }
   let(:kit) { create(:kit) }
-  let(:kit_request) { create(:kit_request, user: user, kit: kit) }
+  let(:regular_user) { create(:user, :regular_user) }
+  let(:kit_request) { create(:kit_request, user: regular_user, kit: kit) }
 
   it "is valid with valid attributes" do
     expect(kit_request).to be_valid
   end
 
   it "is not valid without a valid school_year" do
-    kit_request = build(:kit_request, user: user, school_year: "Not valid")
+    kit_request = build(:kit_request, school_year: "Not valid")
     expect(kit_request).to_not be_valid
   end
 
   it "is not valid without a school_year" do
-    kit_request = build(:kit_request, user: user, school_year: nil)
+    kit_request = build(:kit_request, school_year: nil)
     expect(kit_request).to_not be_valid
   end
 
   it "is not valid without a valid phone number" do
-    kit_request = build(:kit_request, user: user, phone: "333445")
+    kit_request = build(:kit_request, phone: "333445")
     expect(kit_request).to_not be_valid
   end
 
   it "is not valid without a phone number" do
-    kit_request = build(:kit_request, user: user, phone: nil)
+    kit_request = build(:kit_request, phone: nil)
     expect(kit_request).to_not be_valid
   end
 
   it "is not valid without a school_name" do
-    kit_request = build(:kit_request, user: user, school_name: nil)
+    kit_request = build(:kit_request, school_name: nil)
     expect(kit_request).to_not be_valid
   end
 
