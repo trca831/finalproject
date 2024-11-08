523e7495782bedcd4dc780040718ac2e8c3de074
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2024-11-08 11:41:19 -0500

    use file output for development actionmailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,16 +43,16 @@ Rails.application.configure do
   config.action_mailer.default_options = { from: "projectawarend@gmail.com" }
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    user_name: Rails.application.credentials.gmail[:email],
-    password: Rails.application.credentials.gmail[:password],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :file
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "gmail.com",
+  #   user_name: Rails.application.credentials.gmail[:email],
+  #   password: Rails.application.credentials.gmail[:password],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true
+  # }
 
 
   # Print deprecation notices to the Rails logger.
